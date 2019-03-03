cef24aebfc99044b112962de56eec5d6627e0e86
micouy <szpontaniczny@gmail.com>
2019-03-03 16:15:19 +0100

    Minor changes in benchmarks

--- benches/lattice.rs
+++ benches/lattice.rs
@@ -1,3 +1,5 @@
+#![allow(non_snake_case)]
+
 use ::criterion::{criterion_group, criterion_main, Criterion};
 
 use ::ising_lib::prelude::*;
@@ -10,17 +12,20 @@ fn bench_generate_random_spin_index(c: &mut Criterion) {
     });
 }
 
-fn bench_gen_neighbor_indices(c: &mut Criterion) {
-    let mut lattice = Lattice::new(50);
+fn bench_calculate_flip_probability(c: &mut Criterion) {
+    let lattice = Lattice::new(50);
 
-    c.bench_function("generate neighbor indices", move |b| {
-        b.iter(|| lattice.gen_neighbor_indices((420, 42), 1))
+    c.bench_function("calculate flip probability", move |b| {
+        b.iter(|| {
+            let E_diff = lattice.measure_E_diff((10, 42), 1.0);
+            let probability = calc_flip_probability(E_diff, 1.0, 1.0);
+        })
     });
 }
 
 criterion_group!(
     benches,
     bench_generate_random_spin_index,
-    bench_gen_neighbor_indices
+    bench_calculate_flip_probability,
 );
 criterion_main!(benches);
