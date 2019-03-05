c373e0ac50a2dcc6d803db0bb4cc3423b3fd8d84
micouy <szpontaniczny@gmail.com>
2019-03-05 23:30:19 +0100

    Export `calculations::calc_I`

--- src/prelude.rs
+++ src/prelude.rs
@@ -1,4 +1,4 @@
 //! Public exports.
 
-pub use crate::calculations::{calc_X, calc_dE, calc_flip_probability, TRange};
+pub use crate::calculations::{calc_X, calc_dE, calc_I, calc_flip_probability, TRange};
 pub use crate::lattice::Lattice;
